be2f2f59d929d2139ddcb6484af895fee0d9e27b
callistopan <callistopan@gmail.com>
2022-05-22 10:01:17 +0530

    home and room templates created

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
 
-def Home(request):
-    return HttpResponse("Home page")
+rooms=[
+    {'id':1, 'name':'learn python'},
+    {'id':2, 'name':'learn django'},
+    {'id':3, 'name':'learn react'},
+    
+]
+
+def home(request):
+    context={'rooms':rooms}
+    return render(request, 'base/home.html',context)
+
+def room(request,pk):
+    room=None
+    for r in rooms:
+        if r['id']==int(pk):
+            room=r
+    context={'room':room}
+    return render(request, 'base/room.html',context)
