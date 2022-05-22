c2c74950f2067f8950a0a32ebee9c6e1ab69dcee
callistopan <callistopan@gmail.com>
2022-05-22 15:05:05 +0530

    Created Room table

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    #host=
+    #topic
+    name=models.CharField(max_length=200)
+    description=models.TextField(null=True,blank=True) #it can be blank
+    #participants=
+    updated=models.DateTimeField(auto_now=True) #time when the room was updated
+    created=models.DateTimeField(auto_now_add=True) #time when the room was created
+
+    def __str__(self):
+        return self.name
+
+
+
+
