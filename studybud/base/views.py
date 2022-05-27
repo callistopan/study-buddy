2d4efcd103fcc4b5df9ae20d1469b882d73b4ee8
callistopan <callistopan@gmail.com>
2022-05-27 10:25:31 +0530

    advanced search functionality

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render,redirect
 from .models import Room,Topic
 from .forms import RoomForm
+from django.db.models import Q
 
 
 def home(request):
     q=request.GET.get('q') if request.GET.get('q') else ''
-    rooms=Room.objects.filter(topic__name__icontains=q)    #get all rooms from the database
+
+    rooms=Room.objects.filter(Q(topic__name__icontains=q)
+                                |Q(name__icontains=q)
+                                |Q(description__icontains=q))   #get all rooms from the database
     topics=Topic.objects.all()
     context={'rooms':rooms,'topics':topics}
     return render(request, 'base/home.html',context)
