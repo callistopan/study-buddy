99f9883b45b6e44c55b4ecc2d65d8c4a6a9fe7ad
callistopan <callistopan@gmail.com>
2022-05-28 21:48:27 +0530

    signup

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -7,12 +7,15 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.forms import UserCreationForm
 
 
 def loginPage(request):
+    page='login'
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method=='POST':
-        username=request.POST.get('username')
+        username=request.POST.get('username').lower()
         password=request.POST.get('password')
         try:
             user=User.objects.get(username=username)
@@ -26,12 +29,28 @@ def loginPage(request):
             messages.error(request,'Invalid credentials')
 
 
-    context={}
+    context={'page':page}
     return render(request,'base/login_register.html',context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    form=UserCreationForm()
+
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            user= form.save(commit=False) #commit=False means that we don't want to save the user in the database yet
+            user.username=user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect ('home')
+
+        else:
+            messages.error(request,'An error occured during registration')
+    return render(request,'base/login_register.html',{'form':form})
 
 def home(request):
     q=request.GET.get('q') if request.GET.get('q') else ''
