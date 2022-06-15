77924118e44fd2dd8d0ca19d94d46dea82a61efd
callistopan <callistopan@gmail.com>
2022-06-15 07:50:39 +0530

    styling

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -104,6 +104,7 @@ def userProfile(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics=Topic.objects.all()
     if request.method == 'POST':
         print(request.POST)
         form = RoomForm(request.POST)
@@ -113,7 +114,7 @@ def createRoom(request):
             room.save()
             return redirect('home')
 
-    context = {'form': form}
+    context = {'form': form,'topics':topics}
     return render(request, 'base/room_form.html', context)
 
 
